8742d9334df645ed0b91cbadfac94c9a3820e08b
Kushagra Vijan <66737276+KushagraVijan@users.noreply.github.com>
2020-06-24 11:21:06 +0530

    Update pass.js

--- public/assets/pass.js
+++ public/assets/pass.js
@@ -1,1 +1,1 @@
-module.exports = "mongodb+srv://Kushagra:Kushagra%2303@to-do-efp1d.mongodb.net/To-Do?retryWrites=true&w=majority";
+module.exports = "mongodb+srv://<Name>:<Password>@to-do-efp1d.mongodb.net/<Data-Base Name>?retryWrites=true&w=majority";
